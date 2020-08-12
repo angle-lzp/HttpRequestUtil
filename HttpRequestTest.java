37a7e85bec336eb407e4542cb99bdc8c0d879335
angleOne <673970648@qq.com>
2020-08-12 17:15:19 +0800

    提交了http请求的工具类‘ ;
    ;

---
+++ HttpRequestTest.java
@@ -0,0 +1,444 @@
+package com.angle.http;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.httpclient.Cookie;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import sun.net.www.protocol.http.HttpURLConnection;
+
+public class HttpRequestTest {
+
+	public static String sendGetMethod(String url, String param) {
+		String result = "";
+		BufferedReader in = null;
+		try {
+			String urlNameString = url + "?" + param;
+			URL realUrl = new URL(urlNameString);
+			// 打开和URL之间的连接
+			URLConnection connection = realUrl.openConnection();
+			// 设置通用的请求属性
+			connection.setRequestProperty("accept", "*/*");
+			connection.setRequestProperty("connection", "Keep-Alive");
+			connection.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+			connection.setRequestProperty("Accept-Charset", "UTF-8");
+			connection.setRequestProperty("contentType", "UTF-8");
+			connection.setRequestProperty("Content-type", "application/x-www-form-urlencoded;charset=UTF-8");
+			// 建立实际的连接
+			connection.connect();
+			// 获取所有响应头字段
+			Map<String, List<String>> map = connection.getHeaderFields();
+			// 遍历所有的响应头字段
+//            for (String key : map.keySet()) {
+//                System.out.println(key + "--->" + map.get(key));
+//            }
+			// 定义 BufferedReader输入流来读取URL的响应
+			in = new BufferedReader(new InputStreamReader(connection.getInputStream(), "utf-8"));
+			String line;
+			while ((line = in.readLine()) != null) {
+				result += line;
+			}
+		} catch (Exception e) {
+			System.out.println("发送GET请求出现异常！" + e);
+			e.printStackTrace();
+		}
+		// 使用finally块来关闭输入流
+		finally {
+			try {
+				if (in != null) {
+					in.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * post请求（用于请求json格式的参数）
+	 *
+	 * @param url    地址
+	 * @param params json格式的参数
+	 * @return
+	 */
+	public static String doPost(String url, String params) throws Exception {
+
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		HttpPost httpPost = new HttpPost(url);// 创建httpPost
+		httpPost.setHeader("Accept", "application/json");
+		httpPost.setHeader("Content-Type", "application/json");
+		String charSet = "UTF-8";
+		StringEntity entity = new StringEntity(params, charSet);
+		httpPost.setEntity(entity);
+		CloseableHttpResponse response = null;
+
+		try {
+
+			response = httpclient.execute(httpPost);
+			StatusLine status = response.getStatusLine();
+			int state = status.getStatusCode();
+			if (state == HttpStatus.SC_OK) {
+				HttpEntity responseEntity = response.getEntity();
+				String jsonString = EntityUtils.toString(responseEntity);
+				return jsonString;
+			} else {
+				System.out.println("-------请求失败-------");
+			}
+		} finally {
+			if (response != null) {
+				try {
+					response.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			try {
+				httpclient.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * 向指定URL发送GET方法的请求
+	 * 
+	 * @param url   发送请求的URL
+	 * @param param 请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
+	 * @return URL 所代表远程资源的响应结果
+	 */
+	public static String sendGet(String url, String param) {
+		String result = "";
+		BufferedReader in = null;
+		try {
+			String urlNameString = url + "?" + param;
+			URL realUrl = new URL(urlNameString);
+			// 打开和URL之间的连接
+			URLConnection connection = realUrl.openConnection();
+			// 设置通用的请求属性
+			connection.setRequestProperty("accept", "*/*");
+			connection.setRequestProperty("connection", "Keep-Alive");
+			connection.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+			connection.setRequestProperty("Accept-Charset", "UTF-8");
+			connection.setRequestProperty("contentType", "UTF-8");
+			connection.setRequestProperty("Content-type", "application/x-www-form-urlencoded;charset=UTF-8");
+			// 建立实际的连接
+			connection.connect();
+			// 获取所有响应头字段
+//			Map<String, List<String>> map = connection.getHeaderFields();
+//			// 遍历所有的响应头字段
+//			for (String key : map.keySet()) {
+//				System.out.println(key + "--->" + map.get(key));
+//			}
+			result = readStrByCode(connection.getInputStream(), "UTF-8");
+			// 定义 BufferedReader输入流来读取URL的响应
+			in = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
+			String line;
+			while ((line = in.readLine()) != null) {
+				result += line;
+			}
+		} catch (Exception e) {
+			System.out.println("发送GET请求出现异常！" + e);
+			e.printStackTrace();
+		}
+		// 使用finally块来关闭输入流
+		finally {
+			try {
+				if (in != null) {
+					in.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return result;
+	}
+
+	public static String sendGetWithHead(String url, String param, String header, String token) {
+		String result = "";
+		BufferedReader in = null;
+		try {
+			String urlNameString = url + "?" + param;
+			URL realUrl = new URL(urlNameString);
+			// 打开和URL之间的连接
+			URLConnection connection = realUrl.openConnection();
+			// 设置通用的请求属性
+			connection.setRequestProperty("accept", "*/*");
+			connection.setRequestProperty("connection", "Keep-Alive");
+			connection.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+			connection.setRequestProperty("Accept-Charset", "UTF-8");
+			connection.setRequestProperty("contentType", "UTF-8");
+			connection.setRequestProperty("Content-type", "application/x-www-form-urlencoded;charset=UTF-8");
+			connection.setRequestProperty(header, token);
+			// 建立实际的连接
+			connection.connect();
+			// 获取所有响应头字段
+//			Map<String, List<String>> map = connection.getHeaderFields();
+//			// 遍历所有的响应头字段
+//			for (String key : map.keySet()) {
+//				System.out.println(key + "--->" + map.get(key));
+//			}
+			result = readStrByCode(connection.getInputStream(), "UTF-8");
+			// 定义 BufferedReader输入流来读取URL的响应
+			in = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
+			String line;
+			while ((line = in.readLine()) != null) {
+				result += line;
+			}
+		} catch (Exception e) {
+			System.out.println("发送GET请求出现异常！" + e);
+			e.printStackTrace();
+		}
+		// 使用finally块来关闭输入流
+		finally {
+			try {
+				if (in != null) {
+					in.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return result;
+	}
+
+	public static String sendGetCookie(String url, String cs) {
+		String result = "";
+		BufferedReader in = null;
+		try {
+			URL realUrl = new URL(url);
+			// 打开和URL之间的连接
+			URLConnection connection = realUrl.openConnection();
+			// 设置通用的请求属性
+			connection.setRequestProperty("accept", "*/*");
+			connection.setRequestProperty("connection", "Keep-Alive");
+			connection.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+			connection.setRequestProperty("Accept-Charset", "UTF-8");
+			connection.setRequestProperty("contentType", "UTF-8");
+			connection.setRequestProperty("Content-type", "application/x-www-form-urlencoded;charset=UTF-8");
+			connection.setRequestProperty("Cookie", cs);
+			// 建立实际的连接
+			connection.connect();
+			// 获取所有响应头字段
+//	      Map<String, List<String>> map = connection.getHeaderFields();
+//	      // 遍历所有的响应头字段
+//	      for (String key : map.keySet()) {
+//	        System.out.println(key + "--->" + map.get(key));
+//	      }
+			result = readStrByCode(connection.getInputStream(), "UTF-8");
+			// 定义 BufferedReader输入流来读取URL的响应
+			in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			String line;
+			while ((line = in.readLine()) != null) {
+				result += line;
+			}
+		} catch (Exception e) {
+			System.out.println("发送GET请求出现异常！" + e);
+			e.printStackTrace();
+		}
+		// 使用finally块来关闭输入流
+		finally {
+			try {
+				if (in != null) {
+					in.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * 向指定 URL 发送POST方法的请求
+	 * 
+	 * @param url   发送请求的 URL
+	 * @param param 请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
+	 * @return 所代表远程资源的响应结果
+	 * @throws IOException
+	 */
+	public static String sendPost(String url, String param) throws IOException {
+		OutputStreamWriter out = null;
+		BufferedReader in = null;
+		String result = "";
+		try {
+			URL realUrl = new URL(url);
+			// 打开和URL之间的连接
+			URLConnection conn = realUrl.openConnection();
+			// 设置通用的请求属性
+			conn.setConnectTimeout(5000);
+			conn.setRequestProperty("accept", "*/*");
+			conn.setRequestProperty("connection", "Keep-Alive");
+			conn.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+			conn.setRequestProperty("Accept-Charset", "UTF-8");
+			conn.setRequestProperty("contentType", "UTF-8");
+			conn.setRequestProperty("Content-type", "application/x-www-form-urlencoded;charset=UTF-8");
+			// 发送POST请求必须设置如下两行
+			conn.setDoOutput(true);
+			conn.setDoInput(true);
+			// 获取URLConnection对象对应的输出流
+			out = new OutputStreamWriter(conn.getOutputStream(), "UTF-8");
+			// 发送请求参数
+			out.write(param);
+			// flush输出流的缓冲
+			out.flush();
+			out.close();
+			result = readStrByCode(conn.getInputStream(), "UTF-8");
+			// 定义BufferedReader输入流来读取URL的响应
+			in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String line;
+			while ((line = in.readLine()) != null) {
+				result += line;
+			}
+		} catch (Exception e) {
+			System.out.println("发送 POST 请求出现异常！" + e);
+			e.printStackTrace();
+		}
+		// 使用finally块来关闭输出流、输入流
+		finally {
+			try {
+				if (out != null) {
+					out.close();
+				}
+				if (in != null) {
+					in.close();
+				}
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+		return result;
+	}
+
+	public static String send_Post(String strURL, String params) {
+		try {
+			URL url = new URL(strURL);// 创建连接
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setDoOutput(true);
+			connection.setDoInput(true);
+			connection.setUseCaches(false);
+			connection.setInstanceFollowRedirects(true);
+			connection.setRequestMethod("POST"); // 设置请求方式
+			connection.setRequestProperty("Accept", "application/json"); // 设置接收数据的格式
+			connection.setRequestProperty("Content-Type", "application/json"); // 设置发送数据的格式
+			connection.connect();
+			OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream(), "UTF-8"); // utf-8编码
+			out.append(params);
+			out.flush();
+			out.close();
+			// 读取响应
+			int length = (int) connection.getContentLength();// 获取长度
+			InputStream is = connection.getInputStream();
+			if (length != -1) {
+				byte[] data = new byte[length];
+				byte[] temp = new byte[512];
+				int readLen = 0;
+				int destPos = 0;
+				while ((readLen = is.read(temp)) > 0) {
+					System.arraycopy(temp, 0, data, destPos, readLen);
+					destPos += readLen;
+				}
+				String result = new String(data, "UTF-8"); // utf-8编码
+				System.out.println(result);
+				return result;
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "error"; // 自定义错误信息
+	}
+
+	public static String readStrByCode(InputStream is, String code) {
+		StringBuilder builder = new StringBuilder();
+		BufferedReader reader = null;
+		String line = "";
+		try {
+			reader = new BufferedReader(new InputStreamReader(is, code));
+			while ((line = reader.readLine()) != null) {
+
+				builder.append(line);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return builder.toString();
+	}
+
+	public static String xiechengPost(String urlStr, String date) {
+		String linet = "";
+		try {
+			URL url = new URL(urlStr);
+			URLConnection con = url.openConnection();
+			con.setConnectTimeout(5000);
+			con.setDoOutput(true);
+			con.setRequestProperty("Pragma:", "no-cache");
+			con.setRequestProperty("Cache-Control", "no-cache");
+			con.setRequestProperty("Content-Type", "text/xml");
+			// con.setRequestProperty("Accept-Charset", "UTF-8");
+			OutputStreamWriter out = new OutputStreamWriter(con.getOutputStream(), "UTF-8");
+			out.write(date);
+			out.flush();
+			out.close();
+			BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String line = "";
+			for (line = br.readLine(); line != null; line = br.readLine()) {
+				linet = line;
+			}
+		} catch (MalformedURLException e) {
+			// e.printStackTrace();
+			return linet;
+		} catch (IOException e) {
+			// e.printStackTrace();
+			return linet;
+
+		}
+		System.out.println(linet);
+		return linet;
+	}
+
+	private static long create_timestamp() {
+		return System.currentTimeMillis() / 1000;
+	}
+
+	public static String cookie() {
+		HttpClient client = new HttpClient();
+		GetMethod getMethod = new GetMethod("http://wc.wz121.com");
+		Cookie[] cookies = null;
+		try {
+			client.executeMethod(getMethod);
+			cookies = client.getState().getCookies();
+			getMethod.releaseConnection();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		return cookies[0].toString();
+	}
+
+	public static void main(String[] args) throws Exception {
+		//用于测试的数据
+		String jsonData = HttpRequestTest.sendGet("http://xxx.xxx.xxx.xxx:8080/warehouse/findCurrentTasks","pageNum=1&username=郑卡&password=123&lonLat=110.384755,18.797036");
+		System.out.println(jsonData);
+	}
+}
